b46140871ae7ba1c73efa715b86934033008a503
Jack Atherton <jackrrye@gmail.com>
2019-06-04 20:07:46 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def dictionary
-  dictionary_hash = {
+  {
     "hello" => "hi", 
     "to" => "2", 
     "two" => "2", 
@@ -17,7 +17,8 @@ end
 def word_substituter(tweet)
   each_word = tweet.split(" ")
   each_word.each do |word|
-    word_downcase = word.downcase 
-    if dictionary.include?(word_downcase)
-      #somehow replace key with value 
+    if dictionary.include?(word.downcase)
+      word = dictionary[word.downcase]
+    end
+  end
 end
