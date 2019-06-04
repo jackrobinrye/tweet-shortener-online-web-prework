28096a2e3530fc919875c5b73da9b81b3ffb463c
Jack Atherton <jackrrye@gmail.com>
2019-06-04 20:29:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -42,7 +42,7 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    "#{tweet[0..137]}..."
+    "#{tweet[0..136]}..."
   else 
     selective_tweet_shortener(tweet)
   end
